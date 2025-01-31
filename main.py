50d4d71e363ccad289f5e289a224ed45413bfbe9
FeliBog <felibog@bk.ru>
2025-01-31 18:55:02 +0300

    BIG UPDATE

--- main.py
+++ main.py
@@ -6,10 +6,9 @@
 #PLEASE, INSTALL ALL PACKAGES
 #    pip install uuid
 #    pip install requests
-#    pip install gputil
 #    pip install psutil
 #OR 
-#    pip install uuid requests psutil gputil
+#    pip install uuid requests psutil
 #
 #IF CODE DOESN'T WORK, PLEASE, WRITE TO ME: felibog@bk.ru
 
@@ -26,6 +25,8 @@ from uuid import getnode as getmac
 import psutil
 import json
 import GPUtil
+import signal
+import sys
 
 #functions
 def correct_size(bts): #convert bytes
@@ -71,14 +72,30 @@ phisical_cores = psutil.cpu_count(False)
 logic_cores = psutil.cpu_count()
 cpu_usage = psutil.cpu_percent()
 gpus = GPUtil.getGPUs()
-
+disks = psutil.disk_partitions()
+disk_info = dict()
+for i in disks:
+    disk = list()
+    disk.append(i.device)
+    disk.append(i.fstype)
+    size = psutil.disk_usage(i.device)
+    total = size.total; disk.append(total)
+    used = size.used; disk.append(used)
+    free = size.free; disk.append(free)
+    percent = size.percent; disk.append(percent)
+    disk_info[i.device] = disk
+battery = psutil.sensors_battery()
+avaliable_os_signals = str(signal.valid_signals()).replace("{", "").replace("<", "").replace(">", "").replace("}", "").replace(" ", "").replace("Signals.", "").replace(":", "\t——\t").split(",")
+active_users = psutil.users()
 
 #full information of system
 all = {
 "OSname" : osname,
 "OSrelease" : release,
 "OS Version" : version,
+"Avaliable Signals" : avaliable_os_signals,
 "User" : name,
+"Active Users" : active_users,
 "Local IP" : ip,
 "Public IP" : ipinfo["ip"],
 "Host" : host,
@@ -100,7 +117,8 @@ all = {
 "RAM Total" : correct_size(ram.total),
 "RAM Used" : correct_size(ram.used),
 "RAM Free" : correct_size(ram.available),
-"RAM Usage %" : f"{ram.percent}%"
+"RAM Usage %" : f"{ram.percent}%",
+"Battery" : battery
 }
 
 os.system("cls")
@@ -124,8 +142,14 @@ os.system("cls")
 #output to the console
 print("Windows-View \nCopyright (c) 2025 Felix Bogomolov \nGitHub: https://github.com/FeliBog/Windows-View/ \n \nOS:")
 print("\tOS:", all["OSname"], all["OSrelease"], f"({all['OS Version']})")
+print("\tAvaliable Signals:")
+for i in all["Avaliable Signals"]:
+    print("\t\t ", i)
 print("Users:")
 print("\tThis User:", all["User"])
+print("\tActive Users (Loginned In):")
+for i in all["Active Users"]:
+    print(f"\t\t{i.name}")
 print("Connections:")
 print("\tLocal IP:", all["Local IP"])
 print("\tPublic IP:", all["Public IP"])
@@ -141,9 +165,9 @@ print("\tProcessor:", all["Processor"])
 print("\tArchitectre:", all["Architecture"])
 print("\tUsage:", all["CPU Usage %"])
 print("\tProcessor frequency:")
-print("\t\tCurrent:", all["Current Processor Frequency"])
-print("\t\tMin:", all["Min Processor Frequency"])
-print("\t\tMax:", all["Max Processor Frequency"])
+print("\t\tCurrent:", str(all["Current Processor Frequency"]) + " Mhz")
+print("\t\tMin:", str(all["Min Processor Frequency"]) + " Mhz")
+print("\t\tMax:", str(all["Max Processor Frequency"]) + " Mhz")
 print("\tCores:")
 print("\t\tPhysical:", all["Physical Cores"])
 print("\t\tLogical:", all["Logical Cores"])
@@ -163,3 +187,22 @@ for gpu in gpus:
     print(f"\t\t\tFree: {correct_size(gpu.memoryFree*1024*1024)}")
     print(f"\t\tTemperature: {gpu.temperature} °C")
     print(f"\t\tUUID: {gpu.uuid}")
+print("Disks:")
+for i in disk_info:
+    this_disk = disk_info[i]
+    print(f"\t{i}")
+    print(f"\t\tFile System: {this_disk[1]}")
+    print(f"\t\tMemory:")
+    print(f"\t\t\tTotal: {correct_size(this_disk[2])}")
+    print(f"\t\t\tUsed: {correct_size(this_disk[3])}")
+    print(f"\t\t\tFree: {correct_size(this_disk[4])}")
+    print(f"\t\t\tPercent: {this_disk[5]}%")
+print("Power (Battery):")
+print("\tPercent:", str(all["Battery"].percent) + "%")
+if all["Battery"].secsleft != psutil.POWER_TIME_UNLIMITED:
+    if all["Battery"].secsleft/60 < 60: print("\tTime to discharge: ~" + str(all["Battery"].secsleft//60) + " min")
+    else: print("\tTime to discharge: ~" + str(all["Battery"].secsleft//60//60) + " h")
+if all["Battery"].power_plugged == True:
+    print("\tBattery is charging.")
+else:
+    print("\tBattery is not charging.")
